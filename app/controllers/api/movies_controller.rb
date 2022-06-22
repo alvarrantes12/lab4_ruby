d6c978feceee2e2102a1c2502438ab992162b5dc
NicoleGarciaLuna <nicolegarcia86@hotmail.com>
2022-06-22 12:46:56 -0600

    Finish

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,4 +1,60 @@
 module Api
+  class MoviesController < ApplicationController
+    before_action :set_movie, only: %i[ show edit update destroy ]
+    skip_before_action :verify_authenticity_token
+    before_action :set_directors
+
+    def index
+      @movies = Movie.all
+    end
+
+    def show; end
+
+    def new
+      @movie = Movie.new
+    end
+
+    def edit; end
+
+    def create
+      @movie = Movie.new(movie_params)
+      if @movie.save
+        render "api/movies/show", status: :created
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    def update
+      if @movie.update(movie_params)
+        render "api/movies/show", status: :created
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @movie.destroy
+      render "api/movies/show", status: :created
+    end
+
+    private
+
+    def set_movie
+      @movie = Movie.find(params[:id])
+    end
+
+    def set_directors
+      @directors = Director.all.map { |director| ["#{director.name} #{director.last_name}", director.id] }
+    end
+
+    def movie_params
+      params.require(:movie).permit(:name, :year_of_publication, :director_id)
+    end
+
+  end
+end
+
   # class MoviesController < ApplicationController
   #   before_action :set_movie, only: %i[ show edit update destroy ]
 
@@ -69,59 +125,3 @@ module Api
   #       params.require(:movie).permit(:name, :year_of_publication)
   #     end
   # end
-
-  class MoviesController < ApplicationController
-    before_action :set_movie, only: %i[ show edit update destroy ]
-    skip_before_action :verify_authenticity_token
-    before_action :set_directors
-
-    def index
-      @movies = Movie.all
-    end
-
-    def show; end
-
-    def new
-      @movie = Movie.new
-    end
-
-    def edit; end
-
-    def create
-      @movie = Movie.new(movie_params)
-      if @movie.save
-        render "api/movies/show", status: :created
-      else
-        render :new, status: :unprocessable_entity
-      end
-    end
-
-    def update
-      if @movie.update(movie_params)
-        render "api/movies/show", status: :created
-      else
-        render :new, status: :unprocessable_entity
-      end
-    end
-
-    def destroy
-      @movie.destroy
-      render "api/movies/show", status: :created
-    end
-
-    private
-
-    def set_movie
-      @movie = Movie.find(params[:id])
-    end
-
-    def set_directors
-      @directors = Director.all.map { |director| ["#{director.name} #{director.last_name}", director.id] }
-    end
-
-    def movie_params
-      params.require(:movie).permit(:name, :year_of_publication, :director_id)
-    end
-
-  end
-end
