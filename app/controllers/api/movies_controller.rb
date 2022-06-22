9b378696f96c92679f18dd4773d9cd5af8431fb9
Carlos <carlos.guevara@ucrso.info>
2022-06-22 08:07:41 -0600

    Ruby Branch Completed

---
+++ app/controllers/api/movies_controller.rb
@@ -0,0 +1,51 @@
+module Api
+  class MoviesController < ApplicationController
+    before_action :set_movie, only: %i[ show edit update destroy ]
+    skip_before_action :verify_authenticity_token
+
+    def index
+      @movies = Movie.all
+    end
+
+    def show; end
+
+    def new
+      @movie = Movie.new
+    end
+
+    def edit; end
+
+    def create
+      @movie = Movie.new(movie_params)
+      if @movie.save
+        render 'movies/show', status: :created
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    def update
+      if @movie.update(movie_params)
+        render 'movies/show', status: :created
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @movie.destroy
+      render 'movies/show', status: :created
+    end
+
+    private
+
+    def set_movie
+      @movie = Movie.find(params[:id])
+    end
+
+    def movie_params
+      params.require(:movie).permit(:name, :age, :director_id)
+    end
+
+  end
+end
